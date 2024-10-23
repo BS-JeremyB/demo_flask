414c06d89cde24c3adec7f2b718ebbac6ddc4e5d
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-10-23 12:11:53 +0200

    templates & Jinja2

--- app/routes/presentation.py
+++ app/routes/presentation.py
@@ -1,5 +1,13 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
+@app.route('/hello_world/')
 def hello_world():
     return 'Hello World !'
+
+
+@app.route('/')
+def index():
+    title = 'Présentation Flask'
+    to_see = ['jinja2', 'variable', 'filtre', 'condition', 'boucles']
+    return render_template('index.html', title=title, to_see=to_see)
